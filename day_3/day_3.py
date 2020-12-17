334235a7ddf73fe9cbdd0f40e1e8670292a2d047
Vladimir Sumenko <3760097@gmail.com>
2020-12-17 21:47:09 +0300

    day_3 complete

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,13 +1,10 @@
-"""
+﻿"""
     Day #03
     https://adventofcode.com/2020/day/3
 """
 
-# course = (3,1)
 courses = ((1,1), (3,1), (5,1), (7,1),(1,2)) #336
-# courses = ((3,1),(3,1),(3,1),)
-s_open = "."
-s_tree = "#"
+
 mul = 1
 with open("input.txt", "r") as inp:
     lines = inp.readlines()
@@ -16,14 +13,13 @@ with open("input.txt", "r") as inp:
         line_number = 0
         trees = 0    
         for line in lines:
-            line_number += 1
-            if line_number % course[1] + 1:
+            if line_number % (course[1]) == 0:
                 length = len(line)-1
                 pos = X - (X // length) * length
                 char = line[pos]
-                if char == s_tree:
+                if char == "#":
                     trees += 1
                 X += course[0]
+            line_number += 1
         mul *= trees
-        print(f"Trees encounter: {trees}")
     print("Multiply answer:", mul)
