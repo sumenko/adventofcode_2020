b96c7f5e51579a266fee0d0c5910ef59772b056f
Vladimir Sumenko <3760097@gmail.com>
2020-12-18 23:06:03 +0300

    day_4. part 01+02 shorten and complete

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -3,7 +3,7 @@
     Day #04. Is passport valid?
     https://adventofcode.com/2020/day/4
     part 01: 245
-    part 02: ?
+    part 02: 133
 """
 
 
@@ -16,7 +16,6 @@ def is_valid(line, fields):
 
 def is_hcl_correct(hcl):
     """ (Hair Color) - a # followed by exactly six characters 0-9 or a-f. """
-    # TODO
     if hcl[0] == "#" and len(hcl) == 7:
         try:
             int(hcl[1:], 16)
@@ -27,7 +26,7 @@ def is_hcl_correct(hcl):
 
 
 def is_ecl_correct(ecl):
-    print(ecl)
+    """ (Eye Color) - exactly one of: amb blu brn gry grn hzl oth. """
     if ecl in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth"):
         return True
     return False
@@ -35,7 +34,6 @@ def is_ecl_correct(ecl):
 
 def is_pid_correct(pid):
     """ (Passport ID) - a nine-digit number, including leading zeroes. """
-    # TODO
     if len(pid) == 9 and pid.isdigit():
         return True
     return False
@@ -44,7 +42,7 @@ def is_pid_correct(pid):
 def is_hgt_correct(hgt):
     """(Height) a number followed by either cm or in and in interval"""
     interval = {"cm": (150, 193), "in": (59, 76)}
-    height = hgt.split("cm")[0].split("in")[0]
+    height = hgt.split("cm")[0].split("in")[0] # too lo-o-o-ong
     units = hgt[len(height):]
     try:
         if interval[units][0] <= int(height) <= interval[units][1]:
@@ -59,74 +57,66 @@ def is_valid_data(line, fields):
     for field in fields:
         if field in line.keys():
             # check data
-            if field == "ecl":
-                print(field)
-            if not (
-               (field == "byr" and 1920 <= int(line[field]) <= 2002) or
-               (field == "iyr" and 2010 <= int(line[field]) <= 2020) or
-               (field == "eyr" and 2020 <= int(line[field]) <= 2030) or
-               (field == "hgt" and is_hgt_correct(line[field])) or
-               (field == "hcl" and is_hcl_correct(line[field])) or
-               (field == "ecl" and is_ecl_correct(line[field])) or
-               (field == "pid" and is_pid_correct(line[field]))
-                ):
-                # print("Invalid:", field, ": ", line[field])
-                return False
-    # print("Valid:", field, ": ", line[field])
+            if field == "byr" and 1920 <= int(line[field]) <= 2002:
+                continue
+            if field == "iyr" and 2010 <= int(line[field]) <= 2020:
+                continue
+            if field == "eyr" and 2020 <= int(line[field]) <= 2030:
+                continue
+            if field == "hgt" and is_hgt_correct(line[field]):
+                continue
+            if field == "hcl" and is_hcl_correct(line[field]):
+                continue
+            if field == "ecl" and is_ecl_correct(line[field]):
+                continue
+            if field == "pid" and is_pid_correct(line[field]):
+                continue
+            # nothing found - invalid
+        return False
+    # all done - valid
     return True
 
 
 def read_fields(fname="fields.txt"):
+    """ read fields to check """
     fields = ()
     with open(fname, "r") as inp:
         for line in inp.readlines():
             fields += (line.strip().split("(")[0].strip(),)
     return fields
 
+# def check_batch(fname, is_valid_func):
+
 
 if __name__ == "__main__":
     fields = read_fields("fields_enough.txt")
-    
+
     with open("input.txt", "r") as inp:
-        collected = []
-        correct = []
-        iter = 0
-        doc = {}
         valid = 0
-        invalid = 0
+        wait_new_line = False
+        docs_collected = 0
+        doc = {}
+
         for line in inp.readlines():
-            # input(line)
-            iter += 1
             line = line.split("\n")[0]
 
             if line == "":
-                # empty line
-                if is_valid(doc, fields):
-                    correct.append(doc)
-                    valid += 1
-                else:
-                    invalid += 1
-
-                collected.append(doc)
                 doc = {}
-            else:
-                # some data
-                doc.update(
-                    {k: v for k, v in (kv.split(":") for kv in line.split(" "))}
-                    )
-        # TODO: тут надо упростить чтобы не повторять
-        if is_valid(doc, fields):
-            correct.append(doc)
-            collected.append(doc)
-            valid += 1
-            doc = {}
-        status = "err"
-        if valid+invalid == len(collected):
-            iter = 0
-            # for doc in collected:
-            #     iter += 1
-            #     print(f"{iter:>3}", "   ".join((f"{k}:{doc[k]:>10}" for k in sorted(doc.keys()))))
-            status = "ok"
-        print(f"Total/Valid: {len(collected)} / {valid} {status}")
-        # print(1920 <= int('1994') <= 2002)
-        
+                wait_new_line = False
+                docs_collected += 1
+                continue
+            if wait_new_line:
+                continue
+
+            doc.update(
+                {k: v for k, v in (kv.split(":") for kv in line.split(" "))}
+                )
+
+            if is_valid_data(doc, fields) is True:
+                valid += 1
+                wait_new_line = True
+
+        if line is not "":
+            docs_collected += 1
+
+        print(f"Valid/Total: {valid}/{docs_collected}")
